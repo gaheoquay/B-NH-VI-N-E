db5d9c8502d920b1da54bdda986a02ea519628f9
shjn.be <shjn.be.90@gmail.com>
2016-12-26 18:37:50 +0700

    vunt commit

---
+++ CDYT_HoiDap/CDYT_HoiDap/ViewController/Cell/KeyWordTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  KeyWordTableViewCell.swift
+//  CDYT_HoiDap
+//
+//  Created by CDYT on 12/26/16.
+//  Copyright © 2016 CDYT. All rights reserved.
+//
+
+import UIKit
+
+class KeyWordTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
