7d9ee1e93d0616a6b318cd054fd7b5dbcdfd5951
Vu Thanh Nam <vuthanhnam.it@gmail.com>
2016-12-28 09:03:28 +0700

    register token

--- CDYT_HoiDap/CDYT_HoiDap/AppDelegate.swift
+++ CDYT_HoiDap/CDYT_HoiDap/AppDelegate.swift
@@ -17,6 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
 
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
     // Override point for customization after application launch.
+    registerNotification(application: application)
     return true
   }
 
