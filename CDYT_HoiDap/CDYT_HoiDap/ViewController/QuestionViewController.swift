93d42c6425b407d7cb3e4154eaa67cda5018ac9c
quanganh93 <quanganh@QuangAnhs-MacBook-Pro.local>
2017-02-23 16:26:15 +0700

    delete requestSever

--- CDYT_HoiDap/CDYT_HoiDap/ViewController/QuestionViewController.swift
+++ CDYT_HoiDap/CDYT_HoiDap/ViewController/QuestionViewController.swift
@@ -190,36 +190,7 @@ class QuestionViewController: BaseViewController,UITableViewDelegate,UITableView
     }
     
 
-    func requestServer(){
-        let hotParam : [String : Any] = [
-            "Auth": Until.getAuthKey(),
-            ]
-      
-        Until.showLoading()
-        Alamofire.request(GET_LIST_DOCTOR, method: .post, parameters: hotParam, encoding: JSONEncoding.default, headers: nil).responseJSON { (response) in
-            if let status = response.response?.statusCode {
-                if status == 200{
-                    if let result = response.result.value {
-                        let jsonData = result as! [NSDictionary]
-                        
-                        for item in jsonData {
-                            let entity = ListDoctorEntity.init(dictionary: item)
-                            listAllDoctor.append(entity)
-                        }
-                        self.tbQuestion.reloadData()
-                    }
-                }else{
-                    UIAlertController().showAlertWith(vc: self, title: "Thông báo", message: "Có lỗi xảy ra. Vui lòng thử lại sau", cancelBtnTitle: "Đóng")
-                }
-            }else{
-                UIAlertController().showAlertWith(vc: self, title: "Thông báo", message: "Không có kết nối mạng, vui lòng thử lại sau", cancelBtnTitle: "Đóng")
-            }
-            Until.hideLoading()
-            
-        }
-    }
-    
-     
+         
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 1
     }
