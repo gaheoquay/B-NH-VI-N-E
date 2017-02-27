7201f66ab202c29b18935381f93c5ab75a39cdb4
quanganh93 <quanganh@QuangAnhs-MacBook-Pro.local>
2017-02-27 16:30:54 +0700

    fixed scrollview

--- CDYT_HoiDap/CDYT_HoiDap/ViewController/CreateCvViewController.swift
+++ CDYT_HoiDap/CDYT_HoiDap/ViewController/CreateCvViewController.swift
@@ -8,11 +8,10 @@
 
 import UIKit
 
-class CreateCvViewController: UIViewController {
+class CreateCvViewController: BaseViewController {
     
     @IBOutlet weak var lbGender: UILabel!
     @IBOutlet weak var lbDateOfYear: UILabel!
-    @IBOutlet weak var mainScrollView: UIScrollView!
     
     
     
