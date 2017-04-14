f48079fc33de0a43422f3f050964781080a6a914
quanganh93 <quanganh@QuangAnhs-MacBook-Pro.local>
2017-04-14 15:21:41 +0700

    fix bug

---
+++ CDYT_HoiDap/CDYT_HoiDap/Entity/BookingEntity/PatientHistoryEntity.swift
@@ -0,0 +1,13 @@
+//
+//  PatientHistoryEntity.swift
+//  CDYT_HoiDap
+//
+//  Created by QuangAnh on 14/04/2017.
+//  Copyright © 2017 CDYT. All rights reserved.
+//
+
+import UIKit
+
+class PatientHistoryEntity: NSObject {
+
+}
