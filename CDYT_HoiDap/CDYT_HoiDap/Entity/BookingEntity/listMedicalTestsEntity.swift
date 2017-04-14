6af425cda32750a6d3a602f39a02a993bd7603df
quanganh93 <quanganh@QuangAnhs-MacBook-Pro.local>
2017-04-14 15:51:38 +0700

    fix bug

---
+++ CDYT_HoiDap/CDYT_HoiDap/Entity/BookingEntity/listMedicalTestsEntity.swift
@@ -0,0 +1,13 @@
+//
+//  listMedicalTestsEntity.swift
+//  CDYT_HoiDap
+//
+//  Created by QuangAnh on 14/04/2017.
+//  Copyright © 2017 CDYT. All rights reserved.
+//
+
+import UIKit
+
+class listMedicalTestsEntity: NSObject {
+
+}
